ed93e56e081a54e804d285b2f0d4629649f76270
egileekada <egileoniso.ekada@gmail.com>
2025-08-02 21:50:35 +0100

    update

--- src/components/fundraisingComponents/allDonationList.tsx
+++ src/components/fundraisingComponents/allDonationList.tsx
@@ -4,6 +4,7 @@ import { Flex, Grid } from "@chakra-ui/react";
 import { LoadingAnimation } from "../shared";
 import DonationGroupCard from "./card/donationGroupCard";
 import { IDonationGroup } from "@/helpers/models/fundraising";
+import DonationCard from "./card/donationCard";
 
 export default function AllDonationList() {
 
@@ -11,7 +12,7 @@ export default function AllDonationList() {
 
 
     const { results, isLoading: loadingList, ref, isRefetching: refetchingList } = useInfiniteScroller({
-        url: `/fund-raiser-group/search`, limit: 20, filter: "id", name: "donationlist", search: search,
+        url: `/fund-raiser/search`, limit: 20, filter: "id", name: "donationlist", search: search,
         paramsObj: {
             name: search.toLowerCase()
         }
@@ -20,17 +21,17 @@ export default function AllDonationList() {
     return (
         <LoadingAnimation loading={loadingList} refeching={refetchingList} length={results?.length} >
             <Grid w={"full"} templateColumns={['repeat(2, 1fr)', 'repeat(2, 1fr)', 'repeat(3, 1fr)', 'repeat(3, 1fr)', 'repeat(4, 1fr)']} gap={["1", "1", "3"]} >
-                {results?.filter((item: IDonationGroup) => item?.fundRaisers?.length > 0 && item?.fundRaisers[0]?.visibility === "PUBLIC")?.map((item: IDonationGroup, index: number) => {
-                    if (results?.filter((item: IDonationGroup) => item?.fundRaisers?.length > 0 && item?.fundRaisers[0]?.visibility === "PUBLIC")?.length === index + 1) {
+                {results?.map((item: any, index: number) => {
+                    if (results?.length === index + 1) {
                         return (
                             <Flex ref={ref} key={index} >
-                                <DonationGroupCard item={item} />
+                                <DonationCard item={item} />
                             </Flex>
                         )
                     } else {
                         return (
                             <Flex key={index} >
-                                <DonationGroupCard item={item} />
+                                <DonationCard item={item} />
                             </Flex>
                         )
                     }
--- src/components/fundraisingComponents/card/donationCard.tsx
+++ src/components/fundraisingComponents/card/donationCard.tsx
@@ -1,6 +1,7 @@
 import { ProductImageScroller, CircularProgressBar, DeleteBtn, ShareLink } from "@/components/shared";
 import DonationBtn from "@/components/shared/donationBtn";
 import { IDonationList } from "@/helpers/models/fundraising";
+import { SHARE_URL } from "@/helpers/services/urls";
 import { useDetails } from "@/helpers/store/useUserDetails";
 import { capitalizeFLetter } from "@/helpers/utils/capitalLetter";
 import { formatNumberWithK } from "@/helpers/utils/formatNumberWithK";
@@ -8,8 +9,8 @@ import { formatNumber } from "@/helpers/utils/numberFormat";
 import { textLimit } from "@/helpers/utils/textlimit";
 import useCustomTheme from "@/hooks/useTheme";
 import { Flex, Text } from "@chakra-ui/react";
-import moment from "moment"; 
-import { useRouter } from "next/navigation";
+import moment from "moment";
+import { useRouter, useSearchParams } from "next/navigation";
 
 
 export default function DonationCard(
@@ -27,29 +28,37 @@ export default function DonationCard(
     } = useCustomTheme()
 
     const router = useRouter()
+    const query = useSearchParams();
+    const frame = query?.get('frame');
 
     const { userId } = useDetails((state) => state)
 
     const clickHander = (index: string) => {
-        router?.push("/product/details/fundraising/" + index)
+        if (frame) {
+            window.location.href = `${SHARE_URL}/fundraiser?id=${index}`;
+        } else {
+            router?.push("/product/details/fundraising/" + index)
+        }
     }
 
-    return ( 
-        <Flex  flexDir={"column"} pos={"relative"} bgColor={mainBackgroundColor} borderWidth={"1px"} rounded={"10px"} w={"full"} h={"fit-content"} >
+    return (
+        <Flex flexDir={"column"} pos={"relative"} bgColor={mainBackgroundColor} borderWidth={"1px"} rounded={"10px"} w={"full"} h={"fit-content"} >
             {(item?.user?.userId === userId && item?.total === 0) && (
                 <DeleteBtn donation={true} id={item?.id} isOrganizer={item?.user?.userId === userId} name={item?.name} />
             )}
             <Flex onClick={() => clickHander(item?.id)} cursor={"pointer"} w={"full"} h={"fit-content"} pos={"relative"} >
                 <ProductImageScroller images={[item?.bannerImage]} createdDate={moment(item?.createdDate)?.fromNow()} userData={item?.createdBy} />
-                <ShareLink
-                    data={item}
-                    type="DONATION"
-                    // size="18px"
-                    showText={false}
-                    id={item?.id}
-                />
+                {!frame && (
+                    <ShareLink
+                        data={item}
+                        type="DONATION"
+                        // size="18px"
+                        showText={false}
+                        id={item?.id}
+                    />
+                )}
             </Flex>
-            <Flex w={"full"} flexDir={"column"} px={["2", "2", "3"]} pt={["2", "2", "3"]} gap={"2"} pb={["2", "2", userId !== item?.createdBy?.userId && !pasted ? "0px" : "3"]} >
+            <Flex w={"full"} flexDir={"column"} px={["2", "2", "3"]} pt={["2", "2", "3"]} gap={"2"} pb={["2", "2", userId !== item?.createdBy?.userId && !pasted && !frame ? "0px" : "3"]} >
                 <Flex w={"full"} >
                     <Flex w={"full"} alignItems={"start"} flexDir={"column"} >
                         <Text fontSize={"12px"} color={bodyTextColor} >Fundraising Title</Text>
@@ -71,7 +80,7 @@ export default function DonationCard(
                 </Flex>
             </Flex>
 
-            {(userId !== item?.createdBy?.userId && !pasted) && (
+            {(userId !== item?.createdBy?.userId && !pasted && !frame) && (
                 <Flex as={"button"} w={"full"} display={["none", "none", "flex"]} borderTopWidth={"1px"} fontFamily={"14px"} mt={2} px={"3"} fontWeight={"600"} py={"4"} justifyContent={"center"} >
                     <DonationBtn item={item} user={item?.user} />
                 </Flex>
