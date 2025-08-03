3805bfc0af104447c861ded8f6527a1fe42052a0
egileekada <egileoniso.ekada@gmail.com>
2025-08-03 18:27:53 +0100

    capitalize First Letter

--- src/components/shared/description.tsx
+++ src/components/shared/description.tsx
@@ -1,4 +1,5 @@
 "use client"
+import { capitalizeFLetter } from '@/helpers/utils/capitalLetter'
 import { textLimit } from '@/helpers/utils/textlimit'
 import useCustomTheme from '@/hooks/useTheme' 
 import { Flex, Text } from '@chakra-ui/react'
@@ -12,7 +13,7 @@ export default function DescriptionPage({ description, limit, label, width } : {
     return ( 
         <Flex w={width ?? "full"} flexDir={"column"} bgColor={secondaryBackgroundColor} p={"4"} rounded={"16px"} gap={"2"} >
             <Text fontSize={"14px"} fontWeight={"bold"} >{label}</Text>
-            <Text fontSize={"12px"} >{textLimit(description, textSize)}{description?.length > limit && (<span role='button' style={{ fontWeight: "700" }} onClick={() => setTextSize((prev) => prev === description?.length ? 100 : description?.length)} >{description?.length !== textSize ? "more" : "...less"}</span>)}</Text>
+            <Text fontSize={"12px"} >{textLimit(capitalizeFLetter(description), textSize)}{description?.length > limit && (<span role='button' style={{ fontWeight: "700" }} onClick={() => setTextSize((prev) => prev === description?.length ? 100 : description?.length)} >{description?.length !== textSize ? "more" : "...less"}</span>)}</Text>
         </Flex>
     )
 }
