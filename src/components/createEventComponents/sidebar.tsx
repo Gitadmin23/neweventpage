fbfb49a0ebca61f87f1b43de583112e690c550fa
egileekada <egileoniso.ekada@gmail.com>
2025-09-16 13:43:41 +0100

    Enhance Sidebar component with click handling for event editing
    - Added clickHandler function to manage routing for event editing based on selected item.
    - Updated Sidebar layout to include clickable elements for improved user interaction.
    - Ensured proper routing for different event types in edit mode.

--- src/components/createEventComponents/sidebar.tsx
+++ src/components/createEventComponents/sidebar.tsx
@@ -19,6 +19,16 @@ export default function Sidebar() {
     const pathname = usePathname()
     const router = useRouter()
 
+    const clickHandler = (item: string) => {
+        if(pathname?.includes("edit")){
+            if(!item) {
+                router.push(`/product/create/events/edit?id=${id}`)
+            } else {
+                router.push(`/product/create/events/edit?type=${item}&id=${id}`)
+            }
+        }
+    }
+
     return (
         <Flex bgColor={[mainBackgroundColor, mainBackgroundColor, secondaryBackgroundColor]} flexDir={"column"} p={5} w={["full", "full", "344px"]}  >
             <Flex w={"full"} gap={"3"} pb={["2", "2", "0px"]} position={"relative"} justifyContent={"center"} alignItems={"center"} >
@@ -38,7 +48,7 @@ export default function Sidebar() {
                 <Text textAlign={"center"} fontWeight={"600"} >{pathname?.includes("draft") ? "Draft" : pathname?.includes("edit") ? "Edit" : "Create"} Event</Text>
             </Flex>
             <Flex display={["none", "none", "flex"]} w={"full"} flexDir={"column"} gap={"4"} mt={"6"} >
-                <Flex w={"full"} flexDir={"column"} gap={"2"} rounded={"lg"} bgColor={mainBackgroundColor} p={"3"}  >
+                <Flex cursor={"pointer"} onClick={()=> clickHandler("")} w={"full"} flexDir={"column"} gap={"2"} rounded={"lg"} bgColor={mainBackgroundColor} p={"3"}  >
                     <Flex gap={"2"} alignItems={"center"} >
                         <Flex w={"20px"} h={"20px"} borderWidth={"1.5px"} p={"4px"} borderColor={primaryColor} rounded={"full"} >
                             <Flex w={"full"} h={"full"} rounded={"full"} bgColor={primaryColor} />
@@ -48,7 +58,7 @@ export default function Sidebar() {
                     <Text fontWeight={"600"} fontSize={"14px"} >Tell Us More about your Event</Text>
                     <Text fontSize={"12px"} >Whether it's a conference, seminar, or celebration, let us in on the details. Your event matters, and we're here to ensure it gets the spotlight it deserves.</Text>
                 </Flex>
-                <Flex w={"full"} flexDir={"column"} gap={"2"} rounded={"lg"} bgColor={mainBackgroundColor} p={"3"}  >
+                <Flex cursor={"pointer"} onClick={()=> clickHandler("info")} w={"full"} flexDir={"column"} gap={"2"} rounded={"lg"} bgColor={mainBackgroundColor} p={"3"}  >
                     <Flex gap={"2"} alignItems={"center"} >
                         <Flex w={"20px"} h={"20px"} borderWidth={"1.5px"} p={"4px"} borderColor={!type ? headerTextColor : primaryColor} rounded={"full"} >
                             {type && (
@@ -60,7 +70,7 @@ export default function Sidebar() {
                     <Text fontWeight={"600"} fontSize={"14px"} >Discover the Details</Text>
                     <Text fontSize={"12px"} >We will like to know the location and schedule for your upcoming event</Text>
                 </Flex>
-                <Flex w={"full"} flexDir={"column"} gap={"2"} rounded={"lg"} bgColor={mainBackgroundColor} p={"3"}  >
+                <Flex cursor={"pointer"} onClick={()=> clickHandler("ticket")} w={"full"} flexDir={"column"} gap={"2"} rounded={"lg"} bgColor={mainBackgroundColor} p={"3"}  >
                     <Flex gap={"2"} alignItems={"center"} >
                         <Flex w={"20px"} h={"20px"} borderWidth={"1.5px"} p={"4px"} borderColor={type !== "ticket" ? headerTextColor : primaryColor} rounded={"full"} >
                             {type === "ticket" && (
@@ -74,9 +84,9 @@ export default function Sidebar() {
                 </Flex>
             </Flex>
             <Flex w={"full"} fontSize={"14px"} fontWeight={"600"} rounded={"8px"} px={"2"} display={["flex", "flex", "none"]} borderWidth={"1px"} h={"61px"} justifyContent={"space-between"} alignItems={"center"} >
-                <Text color={primaryColor} >Theme</Text>
-                <Text color={!type ? headerTextColor : primaryColor} >Information</Text>
-                <Text color={type !== "ticket" ? headerTextColor : primaryColor} >Ticket</Text>
+                <Text cursor={"pointer"} onClick={()=> clickHandler("")} color={primaryColor} >Theme</Text>
+                <Text cursor={"pointer"} onClick={()=> clickHandler("info")} color={!type ? headerTextColor : primaryColor} >Information</Text>
+                <Text cursor={"pointer"} onClick={()=> clickHandler("ticket")} color={type !== "ticket" ? headerTextColor : primaryColor} >Ticket</Text>
             </Flex>
         </Flex>
     )
